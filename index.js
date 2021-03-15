8fa08cbd6006a55e096e0cee29e89bfc273f67c4
CPMSP <cpetersonmsp@gmail.com>
2021-03-15 16:10:54 -0500

    checkbox data static

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
 const bookShelf = document.querySelector('.bookShelf');
 
-const input = document.querySelector('.addBookContainer');
-const bookSubmit = document.querySelector('.addButton');
+const inputContainer = document.querySelector('.addBookContainer');
+const newBookButton = document.querySelector('.submitButton');
 
-const myLibrary = [
+const demoData = [
 	{
 		title: 'It',
 		author: 'Stephen King',
 		pages: 1093,
 		complete: true,
-		id: 0
+		commentary:
+			"This book is the reason why I don't like clowns or playing near sewer grates."
 	},
 	{
 		title: 'Books of Blood Vol 1',
 		author: 'Clive Barker',
 		pages: 210,
 		complete: false,
-		id: 1
+		commentary:
+			"My brother really liked horror novels and I think this series is supposed to be pretty good.  I'll have to read it sometime soon."
 	},
 	{
 		title: 'Main Street',
 		author: 'Sinclair Lewis',
 		pages: 471,
 		complete: true,
-		id: 2
+		commentary:
+			"I really don't recall reading this book, but from reading the summary online I'll likely have to again in the future.  It was for school, but I kept it because I remember enjoying it."
 	}
 ];
 
-function Book(title, author, pages, complete) {
+const myLibrary = demoData;
+
+// const currentLibrary = () => {
+// 	localStorage.getItem(library);
+// 	if (library.length === 0) {
+// 		return demoData;
+// 	}
+// 	else {
+// 		return localStorage.setItem(library);
+// 	}
+// };
+
+function Book(title, author, pages, complete, commentary) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.complete = complete;
+	this.commentary = commentary;
 }
 
-function addBook(title, author, pages, complete) {
-	let newBook = new Book(title, author, pages, complete);
+function addBook(title, author, pages, complete, commentary) {
+	let newBook = new Book(title, author, pages, complete, commentary);
 	myLibrary.push(newBook);
 	displayBookshelf();
-	console.log(newBook);
 }
 
 function displayBookshelf() {
@@ -48,16 +63,62 @@ function displayBookshelf() {
 	myLibrary.forEach((book) => {
 		let cover = document.createElement('div');
 		cover.classList.add('bookBinding');
-		cover.textContent = book.title;
+
+		let bookTitle = document.createElement('h3');
+		bookTitle.textContent = book.title;
+
+		let bookAuthor = document.createElement('h4');
+		bookAuthor.textContent = `By: ${book.author}`;
+
+		let bookPages = document.createElement('p');
+		bookPages.textContent = `Pages: ${book.pages}`;
+
+		let finishedBook = document.createElement('p');
+		finishedBook.textContent = 'Completed ?';
+
+		let bookComplete = document.createElement('input');
+		bookComplete.type = 'checkbox';
+		book.complete === true
+			? (bookComplete.checked = true)
+			: (bookComplete.checked = false);
+
+		let commentaryTag = document.createElement('p');
+		commentaryTag.textContent = 'Commentary:';
+
+		let bookCommentary = document.createElement('p');
+		bookCommentary.textContent = book.commentary;
+
+		let deleteBook = document.createElement('button');
+		deleteBook.classList.add('deleteButton');
+		deleteBook.textContent = 'Delete';
+
+		// assign unique id available upon display
+		book.id = myLibrary.indexOf(book);
+
 		bookShelf.appendChild(cover);
+
+		cover.append(
+			bookTitle,
+			bookAuthor,
+			bookPages,
+			finishedBook,
+			bookComplete,
+			commentaryTag,
+			bookCommentary,
+			deleteBook
+		);
+
+		deleteBook.addEventListener('click', () => {
+			myLibrary.filter();
+		});
 	});
 }
 
 function submitBook() {
-	bookSubmit.classList.add('hidden');
+	newBookButton.classList.add('hidden');
 
 	let newForm = document.createElement('div');
-	newForm.classList.add('input');
+	newForm.classList.add('inputContainer');
 
 	let title = document.createElement('input');
 	title.type = 'text';
@@ -71,10 +132,15 @@ function submitBook() {
 	pages.type = 'number';
 	pages.placeholder = '# of pages';
 
+	let commentary = document.createElement('input');
+	commentary.type = 'textarea';
+	commentary.placeholder = 'comments / notes';
+
 	let checkboxDiv = document.createElement('div');
 	checkboxDiv.classList.add('checkbox');
 
 	let checkboxPrompt = document.createElement('p');
+	checkboxPrompt.textContent = 'Completed?';
 	checkboxPrompt.classList.add('checkbox');
 
 	let complete = document.createElement('input');
@@ -85,24 +151,25 @@ function submitBook() {
 	submit.type = 'submit';
 	submit.textContent = 'Submit';
 
-	input.appendChild(newForm);
+	inputContainer.appendChild(newForm);
 
-	newForm.appendChild(title);
-	newForm.appendChild(author);
-	newForm.appendChild(pages);
-	newForm.appendChild(checkboxDiv);
+	newForm.append(title, author, pages, commentary, checkboxDiv);
 
-	checkboxDiv.appendChild(checkboxPrompt);
-	checkboxDiv.appendChild(complete);
-	checkboxDiv.appendChild(submit);
+	checkboxDiv.append(checkboxPrompt, complete, submit);
 
 	submit.addEventListener('click', () => {
-		addBook(title.value, author.value, pages.value, complete.value);
-		bookSubmit.classList.remove('hidden');
-		input.removeChild(newForm);
+		addBook(
+			title.value,
+			author.value,
+			pages.value,
+			complete.checked,
+			commentary.value
+		);
+		newBookButton.classList.remove('hidden');
+		inputContainer.removeChild(newForm);
 	});
 }
 
-bookSubmit.addEventListener('click', submitBook);
+newBookButton.addEventListener('click', submitBook);
 
 displayBookshelf();
